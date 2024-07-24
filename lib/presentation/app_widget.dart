862f4e56066c02746226911765c0a33ce3404e79
andikatp <triprasetya_andika@yahoo.com>
2024-07-24 14:00:20 +0700

    gud

--- lib/presentation/app_widget.dart
+++ lib/presentation/app_widget.dart
@@ -1,4 +1,4 @@
-import 'package:ddd_raja/presentation/screens/home/home_page.dart';
+import 'package:ddd_raja/presentation/home/screens/home_page.dart';
 import 'package:flutter/material.dart';
 
 class AppWidget extends StatelessWidget {
