de0eb228437c84f8278693de7bccedc36f25a126
andikatp <triprasetya_andika@yahoo.com>
2024-07-24 13:59:06 +0700

    forgot to save

--- lib/presentation/location/screens/location_page.dart
+++ lib/presentation/location/screens/location_page.dart
@@ -1,5 +1,5 @@
 import 'package:ddd_raja/application/location/location_controller.dart';
-import 'package:ddd_raja/presentation/widgets/location/select_location.dart';
+import 'package:ddd_raja/presentation/location/widgets/select_location.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
